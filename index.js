0226be9c481f518d2a8903f6d3777abe8ceccef6
Charlie Cheever <ccheever@gmail.com>
2014-01-05 05:08:04 -0800

    Instructing users to install npm version of package
    And version bump

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ error = function (boldStatement) {
   console.error(colors.red("You must have fibrous installed *globally and not locally* to use this module"));
   console.error("    " + "      " + " npm uninstall fibrous");
   console.error("    " + colors.grey("[sudo]") + " npm install -g fibrous");
-  console.error("    " + "      " + " npm install ccheever/use-global-fibrous --save");
+  console.error("    " + "      " + " npm install use-global-fibrous --save");
   console.error("");
   console.error("And switch all your require('fibrous') calls to require('use-global-fibrous')");
   console.error(colors.grey("Why? https://github.com/ccheever/use-global-fibrous/blob/master/README.md"));
